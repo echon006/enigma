1dccf8bae46ed84e6ee8fb13363bfe05022b2d73
Eric Chon <echon92@gmail.com>
2021-11-15 12:31:09 -0700

    Completed decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,4 +1,23 @@
 require './lib/enigma'
 
-enigma = Enigma.new
-enigma.decrypt(message, key, date = default_date)
+
+message_folder = ARGV[0]
+decrypted_folder = ARGV[1]
+key_given = ARGV[2]
+date_given = ARGV[3]
+
+
+def decrypt(message_folder, decrypted_folder, key_given, date_given)
+
+  enigma = Enigma.new
+  new_message = File.open(message_folder)
+  read_message_file = new_message.read
+  decrypted_message = enigma.decrypt(read_message_file, key_given, date_given)
+  open_decrypted_folder = File.open(decrypted_folder, "w")
+  update_with_decrypted_message = open_decrypted_folder.write(decrypted_message[:decryption])
+  close_file = open_decrypted_folder.close
+
+  puts "Created 'decrypted.txt' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+end
+
+decrypt(message_folder, decrypted_folder, key_given, date_given)
